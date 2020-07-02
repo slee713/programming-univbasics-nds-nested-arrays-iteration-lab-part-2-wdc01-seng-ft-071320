9818b5fa70402217e07cdc08fdcba735f2528e72
Steven Lee <steven.lee1393@gmail.com>
2020-07-02 00:14:10 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -4,6 +4,8 @@ def min_array (array)
   while counter<array.length do
     if array[counter] < array[counter +1]
       low_num = array[counter]
+    else 
+      low_num = array[counter+1]
     end
     counter += 1
   end
