e1ef9db927a122437a6c1022a402a98ec3feda9f
Steven Lee <steven.lee1393@gmail.com>
2020-07-02 00:13:33 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,12 @@
+def min_array (array)
+  low_num = 0
+  counter = 0
+  while counter<array.length do
+    if array[counter] < array[counter +1]
+      low_num = array[counter]
+    end
+    counter += 1
+  end
+end
+
+array = [ 19, 40, 11, 1432, 13, 143, 543, 30,  43]
